6e89c568c612e2c8868e89d3350f379b323663e6
Viktor Piacka <viktor@piacka.com>
2021-03-26 16:38:32 +0100

    added constants for status codes

---
+++ gemini/constants.go
@@ -0,0 +1,22 @@
+package gemini
+
+const (
+	StatusInput             = 10
+	StatusSensInput         = 11
+	StatusSuccess           = 20
+	StatusTempRedirect      = 30
+	StatusPermRedirect      = 31
+	StatusTempFailure       = 40
+	StatusServerUnavailable = 41
+	StatusCGIError          = 42
+	StatusProxyError        = 43
+	StatusSlowDown          = 44
+	StatusPermFailure       = 50
+	StatusNotFound          = 51
+	StatusGone              = 52
+	StatusProxyReqRefused   = 53
+	StatusBadRequest        = 59
+	StatusClientCertReq     = 60
+	StatusCertNotAuth       = 61
+	StatusCertNotValid      = 62
+)
